60659d89914e93c94a9354d873b33c113af1ec21
illyalaifu <illyalaifu@gmail.com>
2020-08-27 12:52:34 -0400

    Refactored and renamed methods in the uninformed_solver

--- game/uninformed_search.py
+++ game/uninformed_search.py
@@ -1,20 +1,13 @@
 from .state import State
-from typing import Set
 from collections import deque
 import numpy as np
 
 
-"""
-This class implement one of the Uninformed Search algorithm
-You may choose to implement the Breadth-first or Depth-first or
-Iterative-Deepening search algorithm
-
-"""
-
-
 class UninformedSearchSolver:
-    opened = deque()    # Fronteer
-    closed = set()  # Explored
+    """Implements BFS to find a solution to an 8-puzzle problem
+    """
+    opened = deque()
+    closed = set()
     depth = 0
 
     def __init__(self, current: State, target: State):
@@ -22,72 +15,50 @@ class UninformedSearchSolver:
         self.target_state = target
         self.opened.append(current)
 
-    # def check_inclusive(self, s):
-    #     # your code start here
-    #     pass
+    def is_solvable(self) -> bool:
+        """Detects if the current puzzle has a solution
 
-    def is_solvable(self):
+        Returns:
+            bool: if the puzzle is solvable
+        """
         inv_count = 0
-
+        arr = self.current_state.flatten()
         for i in range(0, 9):
             for j in range(i+1, 9):
-                #  // Value 0 is used for empty space
-                if (self.current_state.flatten()[j] and self.current_state.flatten()[i] and self.current_state.flatten()[i] > self.current_state.flatten()[j]):
+                if (arr[j] and arr[i] and arr[i] > arr[j]):
                     inv_count += 1
         return inv_count % 2 == 0
 
-    def state_walk(self):
-        """
-        * four types of walks
-        * best first search
-        *  ↑ ↓ ← → (move up, move down, move left, move right)
-        * the blank tile is represent by '0'
-        """
+    def next_state(self):
+        """Finds next state that the puzzle can be and loads it for processing
 
+        previously known as:'state_walk'
+        """
         observed_state: State = self.opened.popleft()
         if not self.is_solvable():
             print("UNSOLVABLE")
             return
 
-        if observed_state not in self.closed:
+        if np.all(observed_state == self.target_state):
             self.current_state = observed_state
+            return
 
-            self.closed.add(observed_state)
-            neighbors = observed_state.neighbors()
+        self.closed.add(observed_state)
 
-            for neighbor in neighbors:
+        for neighbor in observed_state.neighbors():
+            if neighbor not in self.closed or neighbor not in self.opened:
                 self.opened.append(neighbor)
 
-        return self.opened
-        # if np.all(observed_state == self.target_state):
-        #     return observed_state
-        # self.current_state = observed_state
-
-        # self.closed.append(observed_state)
-
-        # for neighbor in observed_state.neighbors():
-        #     if neighbor not in self.closed or neighbor not in self.opened:
-        #         print("appending")
-        #         self.opened.append(neighbor)
-
-        # possible_states = [
-        #     x
-        #     for x in observed_state.neighbors()
-        #     if x not in self.closed and x not in self.opened
-        # ]
-        # [self.opened.append(x) for x in possible_states]
-
-    # Check the following to make it work properly
-
     def run(self):
+        """Runs the search
+        """
         # output the start state
         print(f"Initial State: \n{self.current_state.tile_seq}")
         print("---------")
         path = 0
 
         while not self.current_state == self.target_state:
-            self.state_walk()
-            # print(succ)
+            self.next_state()
             print(f"State: {path+1}")
             print(self.current_state.tile_seq)
             print("--------")
@@ -99,6 +70,5 @@ class UninformedSearchSolver:
         print("It took ", path, " iterations")
         print("The length of the path is: ", self.current_state.depth)
         # output the goal state
-        target = self.target_state.tile_seq
-        print(target)
+        print(self.target_state.tile_seq)
         print("goal state !!!!!")
