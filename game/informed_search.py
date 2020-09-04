f46a9ed4f2ecbf92224198605f59cdf7dcf356e8
Will St. Onge <ongewill@gmail.com>
2020-09-03 21:58:41 -0400

    Fixed informed search and the heuristics

--- game/informed_search.py
+++ game/informed_search.py
@@ -26,83 +26,83 @@ class InformedSearchSolver:
         self.opened.append(current)
 
     def check_inclusive(self, item: State):
-        """ Check if the generated state is in open or closed. """
+        """ Check if the generated state is in open and/or closed. """
         in_open = 0
         in_closed = 0
         ret = [-1, -1]
-
-        if item in self.opened:
-            in_open = 1
-            ret[1] = self.opened.index(item)
 
         if item in self.closed:
             in_closed = 1
             ret[1] = self.closed.index(item)
 
+        if item in self.opened:
+            in_open = 1
+            ret[1] = self.opened.index(item)
+
         if in_open == 0 and in_closed == 0:
-            ret[0] = 1  # the child is not in open or closed
+            ret[0] = 1  # The child is not in the open or closed list.
         elif in_open == 1 and in_closed == 0:
-            ret[0] = 2  # the child is already in open
+            ret[0] = 2  # The child is in the open list.
         elif in_open == 0 and in_closed == 1:
-            ret[0] = 3  # the child is already in closed
+            ret[0] = 3  # The child is in the closed list.
         return ret
 
-    """ Checks the inclusivity in the open/closed lists and moves the states accordingly. """
     def check_conditions(self, child: State):
+        """ Checks the inclusivity in the open/closed lists and moves the states accordingly.
+
+        Args:
+            `state` - State object
+        """
         flag = self.check_inclusive(child)
 
-        if flag[0] == 1: # State is in neither list
+        # State is in neither list.
+        if flag[0] == 1:
             self.heuristic_test(child)
-            self.openList.append(child)
-        elif flag[0] == 2: # State is in the open list
-            if child.depth < self.current.depth:
-                test = None # Give the state on open the shorter path
-        else: # State is in the closed list
-            if child.depth < self.current.depth:
-                self.closeList.remove(child)
-                self.openList.append(child)
+            self.opened.append(child)
+
+        # State is in the open list.
+        elif flag[0] == 2:
+            if child.depth < self.current_state.depth:
+                self.opened[flag[1]].depth = child.depth
+                
+        # State is in the closed list.
+        else:
+            if child.depth < self.current_state.depth:
+                self.closed.remove(child)
+                self.opened.append(child)
 
     def next_state(self):
-        """Four possible directions (up, down, left, and right).
-        
-        Uses the best first search algorithm. The blank tile is represent by '0'.
-        """
-        # Add closed state
+        """ Uses the best first search algorithm. The blank tile is represent by '0'. """
+        # Move state to closed.
         self.closed.append(self.current_state)
         self.opened.remove(self.current_state)
-        # Move to the next heuristic state
+
+        # Get current states graph.
         current_state = self.current_state.tile_seq
-
-        coordinates = np.argwhere(current_state == 0).flatten()
-        col, row = coordinates[1], coordinates[0]
-
+        (row, col) = np.argwhere(current_state == 0).flatten()
         self.depth += 1
 
-        # Move up
         if (row - 1) >= 0:
             temp_state = self.current_state.move("up")
             self.check_conditions(temp_state)
 
-        # Move down
         if (row + 1) < len(current_state):
             temp_state = self.current_state.move("down")
             self.check_conditions(temp_state)
 
-        # Move left
         if (col - 1) >= 0:
             temp_state = self.current_state.move("left")
             self.check_conditions(temp_state)
 
-        # Move right
         if (col + 1) < len(current_state):
             temp_state = self.current_state.move("right")
             self.check_conditions(temp_state)
 
-        # Sort the open list first by h(n) then g(n)
+        # Sort the open list first by h(n) then g(n).
         self.opened.sort(key=lambda a: a.weight)
         self.current_state = self.opened[0]
 
-    def heuristic_test(self):
+    def heuristic_test(self, state: State):
         """Solve the game using heuristic search strategies
         
         * There are three types of heuristic rules:
@@ -115,35 +115,35 @@ class InformedSearchSolver:
         * g(n) = depth of path length to start state
         * h(n) = (1) + (2) + (3)
         """
-        h1 = self.misplaced_tiles()
-        h2 = self.misplaced_distances()
-        h3 = 2 * self.tile_reversals()
+        h1 = self.misplaced_tiles(state)
+        h2 = self.misplaced_distances(state)
+        h3 = 2 * self.tile_reversals(state)
 
         # Set the heuristic value for current state
-        self.current_state.weight = self.current_state.depth + h1 + h2 + h3
+        state.weight = state.depth + h1 + h2 + h3
 
-    def misplaced_tiles(self) -> int:
+    def misplaced_tiles(self, state: State) -> int:
         """Counts all misplaced tiles
 
         Returns:
             int: misplaced tile count
         """
-        return np.sum(self.current_state.tile_seq != self.target_state.tile_seq)
+        return np.sum(state.tile_seq != self.target_state.tile_seq)
 
-    def misplaced_distances(self) -> int:
+    def misplaced_distances(self, state: State) -> int:
         distance = 0
-        current_tiles = self.current_state.tile_seq
+        current_tiles = state.tile_seq
         target_tiles = self.target_state.tile_seq
         for current_x, current_y in np.ndindex(current_tiles.shape):
             for goal_x, goal_y in np.ndindex(target_tiles.shape):
                 if (
-                    self.current_state[current_y][current_x]
+                    state[current_y][current_x]
                     == self.target_state[goal_y][goal_x]
                 ):
                     distance += abs(current_y - goal_y) + abs(current_x - goal_x)
         return distance
 
-    def tile_reversals(self) -> int:
+    def tile_reversals(self, state: State) -> int:
         """Counts tiles that are reversed to each other
 
         Returns:
@@ -151,14 +151,14 @@ class InformedSearchSolver:
         """
         reversals = 0
 
-        current_tiles = self.current_state.tile_seq
+        current_tiles = state.tile_seq
         for row in range(len(current_tiles)):
             for col in range(len(current_tiles[row])):
                 if row != 2:
-                    if self.current_state[row][col] == self.target_state[row + 1][col]:
+                    if state[row][col] == self.target_state[row + 1][col]:
                         reversals += 1
                 if col != 2:
-                    if self.current_state[row][col] == self.target_state[row][col + 1]:
+                    if state[row][col] == self.target_state[row][col + 1]:
                         reversals += 1
 
         return reversals
@@ -172,7 +172,7 @@ class InformedSearchSolver:
 
         while not self.current_state == self.target_state:
             self.next_state()
-            # print(self.current_state.tile_seq)
+            print(self.current_state.tile_seq)
             path += 1
 
         print("It took ", path, " iterations")
